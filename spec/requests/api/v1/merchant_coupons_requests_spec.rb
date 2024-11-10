a19a11cc6114571a677443233e49756993960d63
Paul Knapp <ppankluap@gmail.com>
2024-11-10 10:14:19 -0600

    Feat: merchant coupon SHOW test.

--- spec/requests/api/v1/merchant_coupons_requests_spec.rb
+++ spec/requests/api/v1/merchant_coupons_requests_spec.rb
@@ -1,1 +1,12 @@
 require "rails_helper"
+
+describe "Merchant Coupon endpoints", :type => :request do 
+
+  it  "can SHOW a Coupon" do 
+    merchant = Merchant.create!(name: "Test Merchant")
+    coupon = merchant.coupons.create!(name: "Buy One Get One 50", coupon_code: "BOGO50", value: 10.00)
+    get "/api/v1/merchants/#{merchant.id}/coupons/#{coupon.id}"
+
+    expect(response).to have_http_status(:ok)
+  end
+end
