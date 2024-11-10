66a16d8d95dca2b3b82c5bf5a0e7b9d9bb56ef5c
Paul Knapp <ppankluap@gmail.com>
2024-11-10 10:13:29 -0600

    Feat: invoice coupons relation for invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,6 +4,7 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :transactions, dependent: :destroy
+  has_many :invoice_coupons
 
   validates :status, inclusion: { in: ["shipped", "packaged", "returned"] }
 end
