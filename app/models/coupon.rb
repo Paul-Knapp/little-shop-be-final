4b79425e7521a174df7c448b2d7f3bb57ca10012
Paul Knapp <ppankluap@gmail.com>
2024-11-10 10:13:06 -0600

    Feat: invoice coupons information

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,5 +3,6 @@ class Coupon < ApplicationRecord
     validates_presence_of :coupon_code, presence: true
     validates_presence_of :value, presence: true
     belongs_to :merchant
-    has_one :invoice
+    has_many :invoice_coupons
+    has_many :invoices, through: :invoice_coupons
 end
