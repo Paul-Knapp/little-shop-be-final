d7141cbb814a3d28d12ebba42f7ebdc5cc275397
Paul Knapp <ppankluap@gmail.com>
2024-11-10 19:43:02 -0600

    Test: tests for create and show methods for coupons.

--- spec/requests/api/v1/merchant_coupons_requests_spec.rb
+++ spec/requests/api/v1/merchant_coupons_requests_spec.rb
@@ -2,11 +2,37 @@ require "rails_helper"
 
 describe "Merchant Coupon endpoints", :type => :request do 
 
+  it "can CREATE a Coupon" do 
+    merchant = Merchant.create!(name: "Test Merchant")
+    name = "Buy One Get One 50"
+    coupon_code = "BOGO50"
+    value = 10.00
+    status = "active"
+    body = {
+      name: name,
+      coupon_code: coupon_code,
+      value: value,
+      status: status
+    }
+
+    post "/api/v1/merchants/#{merchant.id}/coupons", params: body, as: :json
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to have_http_status(:created)
+
+    
+  end
+
   it  "can SHOW a Coupon" do 
     merchant = Merchant.create!(name: "Test Merchant")
-    coupon = merchant.coupons.create!(name: "Buy One Get One 50", coupon_code: "BOGO50", value: 10.00)
+    coupon = merchant.coupons.create!(name: "Buy One Get One 50", coupon_code: "BOGO50", value: 10.00, status: "active")
     get "/api/v1/merchants/#{merchant.id}/coupons/#{coupon.id}"
+    binding.pry
+    
+    json = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to have_http_status(:ok)
+    expect(response).to be_successful
+    expect(json[:data][:type]).to eq("coupon")
+    expect(json[:data][:id]).to eq(coupon.id.to_s)
   end
 end
