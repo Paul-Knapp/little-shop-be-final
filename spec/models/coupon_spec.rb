402f570544f194b3e0b487bff8b73479a0fba699
Paul Knapp <ppankluap@gmail.com>
2024-11-10 19:42:33 -0600

    Feat: add status validation.

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -5,6 +5,7 @@ describe Coupon, type: :model do
         it { should validate_presence_of(:name)}
         it { should validate_presence_of(:coupon_code)}
         it { should validate_presence_of(:value)}
+        it { should validate_presence_of(:status)}
     end
 
     describe 'relationships' do 
